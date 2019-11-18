3af136a6675cd06d3db0bfaceeb872d3b53d923f
Leandre <leandre.blanchard@epitech.eu>
2019-11-19 02:09:32 +0900

    [submodule] and windows

--- build.sh
+++ build.sh
@@ -11,6 +11,12 @@ elif [[ $1 == "rebuild" ]];
 then
     rm -rf build
     $0
+elif [[ $1 == "windows" ]];
+then
+    mkdir -p build
+    conan install .. --build=missing
+    cmake -DCMAKE_BUILD_TYPE=RELEASE ..
+    cmake --build . -- -G "Visual Studio 16 2019" -A x64
 else
     mkdir -p build
     cd build
